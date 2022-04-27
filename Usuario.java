7b8ef27a198b45423ed0b5c2c4c3a3dc495ece79
Bartumu <104488136+Bartumu@users.noreply.github.com>
2022-04-27 10:29:40 +0100

    Create Usuario.java

---
+++ Usuario.java
@@ -0,0 +1,60 @@
+package MiniBiblioteca;
+
+/**
+ *
+ * @author 205531
+ */
+public class Usuario {
+    private String nome;
+    private int idade;
+    private char sexo;
+    private int tel;
+
+    public Usuarios(String nome, int idade, char sexo, int tel) {
+        this.nome = nome;
+        this.idade = idade;
+        this.sexo = sexo;
+        this.tel = tel;
+    }
+  
+
+    public Usuarios() {
+    }
+
+    
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getTel() {
+        return tel;
+    }
+
+    public void setTel(int tel) {
+        this.tel = tel;
+    }
+    
+    
+    
+}
